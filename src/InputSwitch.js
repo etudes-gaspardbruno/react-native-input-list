05d8ae6da32e1bd5e5bceee0207c53a054dcee8e
João Horta <joao@gaspardbruno.com>
2017-08-01 11:35:09 +0100

    PropTypes import changed to come from 'prop-types'

--- src/InputSwitch.js
+++ src/InputSwitch.js
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Text, Switch, View } from 'react-native';
 
 import generateStyles from './styles/styles';
